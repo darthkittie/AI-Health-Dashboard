61a005e81f13be66add480f9fd7726191e76ee28
Elijah Tharakan <60075797+darthkittie@users.noreply.github.com>
2025-07-22 22:27:36 -0400

    fixed api

--- backend/app/gemini_api.py
+++ backend/app/gemini_api.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=" + GEMINI_API_KEY
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + GEMINI_API_KEY
 
 async def get_health_advice(prompt: str) -> str:
     headers = {"Content-Type": "application/json"}
